abaa3bd621cb8b48488395901c7411c3b7f6fff9
Yanni Fraga <yanni.ribeiro.fraga@avanade.com>
2019-02-22 09:59:35 -0300

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Auxiliar from '../../hoc/Auxiliar';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+  render () {
+    return (
+      <Auxiliar>
+        <Burger />
+        <div>Build Controls</div>
+      </Auxiliar>
+    );
+  }
+}
+
+export default BurgerBuilder;
